7ddf679b0cc51ba2d1327e8b2529b909e0668bf9
David Qian <dqian@college.harvard.edu>
2022-12-07 19:20:55 -0500

    progress

--- assignment3base/assignment_code/assignment3/SPHSystem.hpp
+++ assignment3base/assignment_code/assignment3/SPHSystem.hpp
@@ -17,6 +17,43 @@ namespace GLOO
             // initialize the new velocities to 0, since we add components to them for each force
             new_state.velocities = std::vector<glm::vec3>(new_state.positions.size(), glm::vec3(0., 0., 0.));
 
+            std::vector<float> densities = {};
+            for (int i = 0; i < state.positions.size(); i++) {
+                float row = 0;
+                for (int j = 0; j < state.positions.size(); j++) {
+                    if (j != i) {
+                        float r = glm::length(state.positions[i] - state.positions[j]);
+                        if (r < h) {
+                            row += mass * poly6 * std::pow(h * h - r * r, 3);
+                        }
+                    }
+                }
+                densities.push_back(row);
+            }
+
+            std::vector<float> pressures = {};               
+            for (float d : densities) {
+                pressures.push_back(k * (d - 1000));
+                // std::cout << d << std::endl;
+            }
+
+            std::vector<glm::vec3> forces = {};
+            for (int i = 0; i < state.positions.size(); i++) {
+                glm::vec3 f(0., 0., 0.);
+                for (int j = 0; j < state.positions.size(); j++) {
+                    if (j != i) {
+                        float r = glm::length(state.positions[i] - state.positions[j]);
+                        if (r < h) {
+                            // glm::vec3 dir = glm::normalize(state.positions[i] - state.positions[j]);
+                            glm::vec3 dir = (state.positions[i] - state.positions[j]);
+                            f += -dir * mass * poly6grad * (float) std::pow(h * h - r * r, 2);
+                        }
+                    }
+                }
+                forces.push_back(f);
+            }
+            
+
             // velocities become A(x(t), v(t)): gravity, viscous drag,
             // we just add each force component to the new velocity, and divide by mass
             for (int i = 0; i < state.positions.size(); i++)
@@ -30,6 +67,8 @@ namespace GLOO
                 // viscous drag
                 new_state.velocities[i] += state.velocities[i] * (-1.0f * drag_constant);
 
+                new_state.velocities[i] -= mass/densities[i] * forces[i];
+
                 // divide this net force by the mass of particle to get acceleration
                 new_state.velocities[i] /= mass;
             }
@@ -39,8 +78,16 @@ namespace GLOO
 
     private:
         const float mass = 1.;
-        const float gravity = -5.0;
+        const float gravity = -9.8;
         const float drag_constant = 1.0;
+        // std::vector<float> densities = {};
+        // std::vector<float> pressures = {};
+        // smoothing width
+        const float h = 0.5;
+        // water vapor gas constant
+        const float k = 461.52;
+        const float poly6 = 315. / (64 * M_PI * std::pow(h, 9.));
+        const float poly6grad = - 945. / (32 * M_PI * std::pow(h, 9.));
     };
 } // namespace GLOO
 
